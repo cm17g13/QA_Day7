92c9b9186a2458bcdd832982d9518d3c132a518a
Callum McGregor <callummcgregor50@gmail.com>
2018-03-20 17:31:35 +0000

    End of day

--- src/HashMaps/RunnerClass.java
+++ src/HashMaps/RunnerClass.java
@@ -3,16 +3,19 @@ package HashMaps;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class RunnerClass {
 
-	private static String[] words;
+	private static ArrayList<String> words;
+	//private static String[] words; using an array is bad here
 	private static HashMap<String, String> hmap;
 
 	public static void main(String[] args) {
-		//tells us to use array, but we dont know the size just yet, so mi going to say 100
-		words = new String[100];
+		//tells us to use array, but we don't know the size just yet, so ii going to say 100
+		words = new ArrayList<String>();
 		hmap = new HashMap<String, String>();
 		readFromFile();
 		sortArray();
@@ -22,11 +25,11 @@ public class RunnerClass {
 		// assumes that we still have access to the people from the personList
 		BufferedReader br;
 		try {
-			br = new BufferedReader(new FileReader("TestFile.txt"));
-			int index = 0;
-			while (br.readLine() != null) {
-				words[index] = br.readLine();
-				index++;
+			br = new BufferedReader(new FileReader("C:/Users/Admin/eclipse-workspace/QA_Day7/src/HashMaps/TestFile.txt"));
+			String currentLine = br.readLine();
+			while (currentLine != null) {
+				words.add(currentLine);
+				currentLine = br.readLine();
 			}
 			br.close();
 		} catch (IOException e) {
@@ -35,9 +38,14 @@ public class RunnerClass {
 	}
 	
 	private static void sortArray() {
-		for(int i = 0; i < words.length; i++) {
-			words[i];
+		for(int i = 0; i < words.size(); i++) {
+						
+			System.out.println("Before sorting string: " + words.get(i));
+			
+			char[] current = words.get(i).toLowerCase().trim().toCharArray();
+			Arrays.sort(current);
+			String currentString = String.valueOf(current);
+			System.out.println("After sorting string: " + currentString);
 		}
 	}
-
 }
